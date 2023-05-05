84a5693e1e798bd218c0c192e4f3eda45bce419e
Basil Njoga <Basilnjoga@gmail.com>
2023-05-05 14:43:13 +0300

    created templates and added bootstrap with navbar

--- mark2day/events/urls.py
+++ mark2day/events/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('<int:year>/<str:month>/', views.home, name="home"),
+
 ]
--- mark2day/events/views.py
+++ mark2day/events/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
 # Create views are here
 
-def home(request):
-    return render(request, 'home.html', {})
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    name = "Basil"
+    month = month.capitalize()
+    # convert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number = int(month_number)
+
+    # create a calendar
+    cal = HTMLCalendar().formatmonth(
+        year,
+        month_number )
+    # Get current year
+    now = datetime.now()
+    current_year = now.year 
+
+    # Get current time
+    time = now.strftime('%I:%M %p')
+    return render(request, 'events/home.html', {
+        "name": name,
+        "year": year,
+        "month": month,
+        "month_number": month_number,
+        "cal": cal,
+        "current_year": current_year,
+        "time": time,
+    })
