a0627789ba6891800db3169ff34dbea537fda933
Basil Njoga <Basilnjoga@gmail.com>
2023-05-08 23:21:50 +0300

    created filters and updated admin page view

--- mark2day/events/admin.py
+++ mark2day/events/admin.py
@@ -6,8 +6,8 @@ from .models import Event
 # Register your models here.
 
 #admin.site.register(Venue)
-#admin.site.register(MyClubUser)
-admin.site.register(Event)
+admin.site.register(MyClubUser)
+#admin.site.register(Event)
 
 
 @admin.register(Venue)
@@ -20,3 +20,5 @@ class VenueAdmin(admin.ModelAdmin):
 class EventAdmin(admin.ModelAdmin):
     fields = (('name', 'venue'), 'event_date', 'description', 'manager')
     list_display = ('name', 'event_date', 'venue')
+    list_filter = ('event_date','venue')
+    ordering = ('-event_date',)
